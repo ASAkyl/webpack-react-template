12b10b6329068cb1e727799f18bc9805b2a515c6
ASAkyl <mansurov.akylbek@protonmail.com>
2020-09-23 11:06:08 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import ReactDOM from "react-dom";
 import Form from "./js/components/Form";
 
+const wrapper = document.getElementById("container");
+wrapper ? ReactDOM.render(<Form />, wrapper) : false;
